dbab7c54eb5430f3d03f9bb3b26459d657b5e06c
unknown <ishtiak.shah@gmail.com>
2015-10-25 23:07:31 -0400

    adding factory test case.WIP

--- FrontEnd/src/app/home/home.spec.js
+++ FrontEnd/src/app/home/home.spec.js
@@ -1,40 +1,41 @@
 'use strict';
 
 var expect = require('chai').expect;
-// var x = require('angular-mocks');
+var mock = require('angular-mocks');
+
 (function(){
 	describe("@dev", function(){
 		it('running test at home', function(mochaDone){
-			//expect(2).to.equal(2);
+			expect(2).to.equal(2);
 			mochaDone();
 		});
 
 		it('running test2 at home', function(mochaDone){
-			//expect(2).to.equal(92);
+			expect(2).not.to.equal(92);
 			mochaDone();
 		});
 	});
 
-	// describe('@dev: HomeFactory', function(){
-	// 	var httpBackend, rootScope, homeFactory;
+	describe('@dev: HomeFactory', function(){
+		var httpBackend, scope, homeFactory;
+        
+		beforeEach(angular.mock.module('triviaApp'));
 
-	// 	beforeEach(angular.mock.module('triviaApp'));
+		beforeEach(inject(function(_$rootScope_,_$httpBackend_,HomeFactory){
+			httpBackend = _$httpBackend_;
+			scope = _$rootScope_.$new();
+			homeFactory = HomeFactory;
+		}));
 
-	// 	beforeEach(inject(function(_$rootScope_,_$httpBackend_,_HomeFactory_){
-	// 		httpBackend = _$httpBackend_;
-	// 		scope = _$rootScope_.$new();
-	// 		homeFactory = HomeFactory;
-	// 	}));
+		afterEach(function(){
+			httpBackend.verifyNoOutstandingExpectation();
+			httpBackend.verifyNoOutstandingRequest();
+		});
 
-	// 	afterEach(function(){
-	// 		httpBackend.verifyNoOutstandingExpectation();
-	// 		httpBackend.verifyNoOutstandingRequest();
-	// 	});
-
-	// 	// it('getAll user - gets the list of all users', function(){
-	// 	// 	httpBackend.expectGet('http://localhost:6204/userdata/getall');
-	// 	// 	homeFactory.getAllUser();
-	// 	// 	httpBackend.flush();
-	// 	// });
-	// });
+		it('getAll user - gets the list of all users', function(){
+			//httpBackend.expectGet('http://localhost:6204/userdata/getall');
+			//homeFactory.getAllUser();
+			//httpBackend.flush();
+		});
+	});
 })();
